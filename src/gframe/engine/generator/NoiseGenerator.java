ebfbba7f78f4e83dc6967bd239098afa9b10ba01
conradplake <conrad.plake@gmail.com>
2016-10-24 19:44:16 +0200

    Add files via upload

---
+++ src/gframe/engine/generator/NoiseGenerator.java
@@ -0,0 +1,168 @@
+package gframe.engine.generator;
+
+public class NoiseGenerator {
+
+	public static float[][] generateWhiteNoise(int width, int height) {
+		float[][] noise = new float[width][height];
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				noise[i][j] = (float) Math.random() % 1;
+			}
+		}
+
+		return noise;
+	}
+
+	public static float[][] generateSmoothNoise(float[][] baseNoise, int octave) {
+		int width = baseNoise.length;
+		int height = baseNoise[0].length;
+
+		float[][] smoothNoise = new float[width][height];
+
+		int samplePeriod = 1 << octave; // calculates 2 ^ k
+		float sampleFrequency = 1.0f / samplePeriod;
+
+		for (int i = 0; i < width; i++) {
+			// calculate the horizontal sampling indices
+			int sample_i0 = (i / samplePeriod) * samplePeriod;
+			int sample_i1 = (sample_i0 + samplePeriod) % width; // wrap around
+			float horizontal_blend = (i - sample_i0) * sampleFrequency;
+
+			for (int j = 0; j < height; j++) {
+				// calculate the vertical sampling indices
+				int sample_j0 = (j / samplePeriod) * samplePeriod;
+				int sample_j1 = (sample_j0 + samplePeriod) % height; // wrap
+																		// around
+				float vertical_blend = (j - sample_j0) * sampleFrequency;
+
+				// blend the top two corners
+				float top = interpolate(baseNoise[sample_i0][sample_j0], baseNoise[sample_i1][sample_j0],
+						horizontal_blend);
+
+				// blend the bottom two corners
+				float bottom = interpolate(baseNoise[sample_i0][sample_j1], baseNoise[sample_i1][sample_j1],
+						horizontal_blend);
+
+				// final blend
+				smoothNoise[i][j] = interpolate(top, bottom, vertical_blend);
+			}
+		}
+
+		return smoothNoise;
+	}
+
+	public static float interpolate(float x0, float x1, float alpha) {
+		return x0 * (1 - alpha) + alpha * x1;
+	}
+
+	float[][] generatePerlinNoise(float[][] baseNoise, int octaveCount) {
+		int width = baseNoise.length;
+		int height = baseNoise[0].length;
+
+		float[][][] smoothNoise = new float[octaveCount][][]; // an array of 2D
+																// arrays
+																// containing
+
+		float persistance = 0.5f;
+
+		// generate smooth noise
+		for (int i = 0; i < octaveCount; i++) {
+			smoothNoise[i] = generateSmoothNoise(baseNoise, i);
+		}
+
+		float[][] perlinNoise = new float[width][height];
+		float amplitude = 1.0f;
+		float totalAmplitude = 0.0f;
+
+		// blend noise together
+		for (int octave = octaveCount - 1; octave >= 0; octave--) {
+			amplitude *= persistance;
+			totalAmplitude += amplitude;
+
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
+				}
+			}
+		}
+
+		// normalisation
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				perlinNoise[i][j] /= totalAmplitude;
+			}
+		}
+
+		return perlinNoise;
+	}
+
+	
+	public static double improvedPerlinNoise(double x) {
+		return improvedPerlinNoise(x, 0);
+	}
+
+	public static double improvedPerlinNoise(double x, double y) {
+		return improvedPerlinNoise(x, y, 0);
+	}
+	
+
+	public static double improvedPerlinNoise(double x, double y, double z) {
+		int X = (int) Math.floor(x) & 255, // FIND UNIT CUBE THAT
+				Y = (int) Math.floor(y) & 255, // CONTAINS POINT.
+				Z = (int) Math.floor(z) & 255;
+		x -= Math.floor(x); // FIND RELATIVE X,Y,Z
+		y -= Math.floor(y); // OF POINT IN CUBE.
+		z -= Math.floor(z);
+		double u = fade(x), // COMPUTE FADE CURVES
+				v = fade(y), // FOR EACH OF X,Y,Z.
+				w = fade(z);
+		int A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z, // HASH COORDINATES
+															// OF
+				B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z; // THE 8
+																	// CUBE
+																	// CORNERS,
+
+		return lerp(w,
+				lerp(v, lerp(u, grad(p[AA], x, y, z), // AND ADD
+						grad(p[BA], x - 1, y, z)), // BLENDED
+				lerp(u, grad(p[AB], x, y - 1, z), // RESULTS
+						grad(p[BB], x - 1, y - 1, z))), // FROM 8
+				lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1), // CORNERS
+						grad(p[BA + 1], x - 1, y, z - 1)), // OF CUBE
+						lerp(u, grad(p[AB + 1], x, y - 1, z - 1), grad(p[BB + 1], x - 1, y - 1, z - 1))));
+	}
+
+	static double fade(double t) {
+		return t * t * t * (t * (t * 6 - 15) + 10);
+	}
+
+	static double lerp(double t, double a, double b) {
+		return a + t * (b - a);
+	}
+
+	static double grad(int hash, double x, double y, double z) {
+		int h = hash & 15; // CONVERT LO 4 BITS OF HASH CODE
+		double u = h < 8 ? x : y, // INTO 12 GRADIENT DIRECTIONS.
+				v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+		return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+	}
+
+	static final int p[] = new int[512], permutation[] = { 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194,
+			233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26,
+			197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168,
+			68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220,
+			105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208,
+			89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250,
+			124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
+			223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39,
+			253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238,
+			210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157,
+			184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243,
+			141, 128, 195, 78, 66, 215, 61, 156, 180 };
+
+	static {
+		for (int i = 0; i < 256; i++)
+			p[256 + i] = p[i] = permutation[i];
+	}
+}
---
+++ src/gframe/engine/generator/TextureGenerator.java
@@ -0,0 +1,649 @@
+package gframe.engine.generator;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Polygon;
+import java.awt.Toolkit;
+import java.io.File;
+
+import gframe.engine.Toolbox;
+import gframe.engine.Vector3D;
+import graph.Graph;
+import graph.Node;
+import imaging.ImageHelper;
+import imaging.ImageRaster;
+import imaging.draw.Tool;
+
+public class TextureGenerator {
+
+	
+	/**
+	 * Returns a new texture scaled down by half the width and height of the original texture. 
+	 * Each new pixel thus represents 4 original pixels by averaging their values. 
+	 * */
+	public static ImageRaster mipmap(ImageRaster texture){
+		ImageRaster mipmapped = new ImageRaster(texture.getWidth()/2, texture.getHeight()/2);
+				
+		for(int x=0;x<mipmapped.getWidth();x++){
+			for(int y=0;y<mipmapped.getHeight();y++){
+				
+				// pixel = avg über alle 4 nachbarn im original			
+				int pixel_a = texture.getPixel(x*2, y*2);
+				int pixel_b = texture.getPixel(x*2+1, y*2);
+				int pixel_c = texture.getPixel(x*2, y*2+1);
+				int pixel_d = texture.getPixel(x*2+1, y*2+1);
+								
+				int a_red = (pixel_a >> 16) & 0xff;
+				int b_red = (pixel_b >> 16) & 0xff;
+				int c_red = (pixel_c >> 16) & 0xff;
+				int d_red = (pixel_d >> 16) & 0xff;				
+				int new_red = (a_red+b_red+c_red+d_red) / 4;
+				
+				int a_green = (pixel_a >> 8) & 0xff;
+				int b_green = (pixel_b >> 8) & 0xff;
+				int c_green = (pixel_c >> 8) & 0xff;
+				int d_green = (pixel_d >> 8) & 0xff;
+				int new_green = (a_green+b_green+c_green+d_green) / 4;
+				
+				int a_blue = (pixel_a >> 0) & 0xff;
+				int b_blue = (pixel_b >> 0) & 0xff;
+				int c_blue = (pixel_c >> 0) & 0xff;
+				int d_blue = (pixel_d >> 0) & 0xff;
+				int new_blue = (a_blue+b_blue+c_blue+d_blue) / 4;
+														
+				int pixel =	((255 & 0xFF) << 24) |    
+						((new_red & 0xFF) << 16) |
+		                ((new_green & 0xFF) << 8)  |
+		                ((new_blue & 0xFF) << 0);					
+				
+				mipmapped.setPixel(x, y, pixel);
+			}
+		}
+		
+		return mipmapped;
+	}
+	
+	
+	public static ImageRaster generateMengerSpongeTexture(ImageRaster raster, int pos_x, int pos_y, int width, int cubeColor, int gapColor) {
+		
+		if(width%3!=0){
+			System.out.println("TextureGenerator.generateMengerSpongeTexture: Warning! Parameter width="+width+" is no multiple of 3!");
+		}
+		
+		int blocksize = width / 3;
+				 		
+		for(int s=0;s<3;s++){
+			for(int t=0;t<3;t++){
+				
+				int c = cubeColor;				
+				if(s==1&&t==1){
+					c = gapColor;
+				}
+				
+				if(blocksize % 3 == 0  && c == cubeColor){
+					generateMengerSpongeTexture(raster, pos_x + (s*blocksize), pos_y + (t*blocksize), blocksize, cubeColor, gapColor);
+				}else{
+					for(int x=0;x<blocksize;x++){
+						for(int y=0;y<blocksize;y++){
+							raster.setPixel(pos_x + (s*blocksize + x), pos_y + (t*blocksize + y), c);
+						}
+					}	
+				}
+			}	
+		}
+				
+		return raster;		
+	}
+	
+	
+	public static ImageRaster generateMengerSpongeNormalMap(int width) {
+		ImageRaster normalMap = generateDefaultNormalMap(width, width);
+		return generateMengerSpongeNormalMap(normalMap, 0, 0, width);
+	}
+	
+	
+	public static ImageRaster generateMengerSpongeNormalMap(ImageRaster raster, int pos_x, int pos_y, int width) {
+		
+		if(width%3!=0){
+			System.out.println("TextureGenerator.generateMengerSpongeNormalMap: Warning! Parameter width="+width+" is no multiple of 3!");
+		}
+		
+		int blocksize = width / 3;
+		
+		Polygon left = new Polygon();
+		left.addPoint(0, 0);
+		left.addPoint(blocksize/5, blocksize/5);
+		left.addPoint(blocksize/5, 4 * blocksize/5);
+		left.addPoint(0, blocksize);
+		
+		Polygon right = new Polygon();
+		right.addPoint(blocksize, 0);
+		right.addPoint(4*blocksize/5, blocksize/5);
+		right.addPoint(4*blocksize/5, 4*blocksize/5);
+		right.addPoint(blocksize, blocksize);
+		
+		Polygon up = new Polygon();
+		up.addPoint(0, 0);
+		up.addPoint(blocksize, 0);
+		up.addPoint(4*blocksize/5, blocksize/5);
+		up.addPoint(blocksize/5, blocksize/5);
+		
+		Polygon down = new Polygon();
+		down.addPoint(0, blocksize);
+		down.addPoint(blocksize/5, 4*blocksize/5);
+		down.addPoint(4*blocksize/5, 4*blocksize/5);
+		down.addPoint(blocksize, blocksize);
+		
+		for(int s=0;s<3;s++){
+			for(int t=0;t<3;t++){
+				
+				boolean isGap = false;
+				if(s==1&&t==1){
+					isGap = true;
+				}
+				
+				if(blocksize % 3 == 0  && !isGap){ // tileable?
+					generateMengerSpongeNormalMap(raster, pos_x + (s*blocksize), pos_y + (t*blocksize), blocksize);
+				}
+				else if(isGap){									
+					
+					for(int x=0;x<blocksize;x++){
+						for(int y=0;y<blocksize;y++){
+														
+							Vector3D normal = new Vector3D(0, 0, 1); // default normal points to z direction
+							
+							if(blocksize<5){
+//								normal = Toolbox.getXrotMatrix(180).transform(normal);
+								normal = new Vector3D();
+							}
+							else{
+								if(left.contains(x,y)){
+									normal = Toolbox.getYrotMatrix(-90).transform(normal);
+									if(blocksize<10){
+										normal = Toolbox.getYrotMatrix(-60).transform(normal); // vector "überdrehen" um die helligkeit zu dimmen
+									}
+								}
+								else if(right.contains(x,y)){
+									normal = Toolbox.getYrotMatrix(90).transform(normal);
+									if(blocksize<10){
+										normal = Toolbox.getYrotMatrix(60).transform(normal);
+									}
+								}
+								else if(up.contains(x,y)){
+									normal = Toolbox.getXrotMatrix(-90).transform(normal);
+									if(blocksize<10){
+										normal = Toolbox.getXrotMatrix(-60).transform(normal);
+									}
+								}
+								else if(down.contains(x,y)){
+									normal = Toolbox.getXrotMatrix(90).transform(normal);
+									if(blocksize<10){
+										normal = Toolbox.getXrotMatrix(60).transform(normal);
+									}
+								}else{
+//									normal = Toolbox.getXrotMatrix(180).transform(normal);
+									normal = new Vector3D();
+								}	
+							}
+														
+														
+							int col = toColor(normal);							
+							raster.setPixel(pos_x + (s*blocksize + x), pos_y + (t*blocksize + y), col);
+						}
+					}	
+				}
+			}	
+		}
+				
+		return raster;		
+	}
+	
+	
+	
+	
+	/*
+	 * see also: https://fgiesen.wordpress.com/2010/03/28/how-to-generate-cellular-textures/
+	 * 
+	 * 	
+	 */
+	public static ImageRaster generateVoronoiTexture(int w, int h, int numberOfCells, Color[] colorPalette) {
+		ImageRaster result = new ImageRaster(w, h);
+		
+		if(colorPalette==null){
+			colorPalette = new Color[numberOfCells];
+			for(int i=0;i<numberOfCells;i++){				
+				int random_r = (int)(Math.random()*256);
+				int random_g = (int)(Math.random()*256);			
+				int random_b = (int)(Math.random()*256);				
+				colorPalette[i] = new Color(random_r, random_g, random_b);				
+			}
+		}
+		
+		// build mesh graph		
+		Graph graph = new Graph(w * h);
+		
+		long distToDirectNeighbor = 1000;
+		long distToDiagonalNeighbor = 1414;
+//		long distToDiagonalNeighbor = manhattanDistance? 2000 : 1414; // do euclidian else					
+				
+		
+		for(int y=0;y<h;y++){
+			for(int x=0;x<w;x++){			
+		
+//				long noise = (long)(100 * NoiseGenerator.improvedPerlinNoise((float)w/x, (float)h/y, 0));				
+//				System.out.println("noise = "+noise);
+				long noise = 0L;
+				
+				int nodeId = 1 + y*w+x; // graph node ids start counting at with 1, hence: 1 + ...
+				
+				// oben
+				if(y>0){
+					int nodeIdNeighbor = 1 + (y-1)*w+x;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDirectNeighbor+noise);
+				}
+				
+				// unten
+				if(y<h-1){
+					int nodeIdNeighbor = 1 + (y+1)*w+x;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDirectNeighbor+noise);
+				}
+				
+				// links
+				if(x>0){
+					int nodeIdNeighbor = 1 + (y)*w+x-1;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDirectNeighbor+noise);
+				}
+				
+				// rechts
+				if(x<w-1){
+					int nodeIdNeighbor = 1 + (y)*w+x+1;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDirectNeighbor+noise);
+				}
+											
+				
+				// oben links
+				if(y>0 && x>0){
+					int nodeIdNeighbor = 1 + (y-1)*w+x-1;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDiagonalNeighbor+noise);
+				}
+				
+				// oben rechts
+				if(y>0 && x<w-1){
+					int nodeIdNeighbor = 1 + (y-1)*w+x+1;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDiagonalNeighbor+noise);
+				}
+				
+				// unten links
+				if(y<h-1 && x>0){
+					int nodeIdNeighbor = 1 + (y+1)*w+x-1;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDiagonalNeighbor+noise);
+				}
+				
+				// unten rechts
+				if(y<h-1 && x<w-1){
+					int nodeIdNeighbor = 1 + (y+1)*w+x+1;
+					graph.addEdge(nodeId, nodeIdNeighbor, distToDiagonalNeighbor+noise);
+				}
+			}	
+		}
+		
+		// randomly select terminal nodes (cells)		
+		int[] terminalNodeIds = new int[numberOfCells];
+		for(int i=0;i<numberOfCells;i++){
+			int random_x = (int)(Math.random()*w);
+			int random_y = (int)(Math.random()*h);			
+			int nodeId = 1 + random_y*w + random_x;			
+			graph.setTerminal(nodeId, true);			
+			terminalNodeIds[i] = nodeId;
+		}
+						
+		
+		// color each node/pixel with nearest terminal's color
+//		System.out.println(">> TextureGeneraor: computing voronoi areas");
+		graph.markVoronoiAreas();
+//		System.out.println("<< TextureGeneraor: computing voronoi areas");
+		
+		for(int y=0;y<h;y++){
+			for(int x=0;x<w;x++){						
+				int nodeId = 1 + y*w+x;
+				Node node = graph.getNode(nodeId);
+				int cellNodeId = node.getColor();
+				
+				int colorIndex = -1;
+				for(int i=0;i<terminalNodeIds.length;i++){
+					if(terminalNodeIds[i]==cellNodeId){
+						colorIndex = i;
+					}
+				}				
+				Color color = colorPalette[colorIndex];				
+				result.setPixel(x, y, color.getRGB());
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	
+	public static ImageRaster generateTileTexture(int w, int h, int tilesize, int tileColor, int fugenColor) {
+	
+		ImageRaster result = new ImageRaster(w, h);
+	
+		int tile_size=tilesize;		
+		
+		int numberOfRows = h / tile_size;
+		int tilesPerRow = w / tile_size;
+	
+		
+		for(int r=0;r<numberOfRows;r++){
+			
+			for(int t=0;t<tilesPerRow;t++){
+				
+				for(int y=0;y<tile_size;y++){
+					for(int x=0;x<tile_size;x++){
+														
+						int col = tileColor;
+						
+//						if(Math.random()<0.0005){				// flecken :)
+//							col = fugenColor.getRGB();
+//						}
+												
+						if(x==tile_size-1){ // rechter rand
+							col = fugenColor;
+						}else if(x==0){ // linker rand
+							col = fugenColor;
+						}
+						
+						if(y==tile_size-1){ // unterer rand
+							col = fugenColor;							
+						}else if(y==0){ // oberer rand
+							col = fugenColor;							
+						}		
+						
+						//if(x%tile_size==0 || y%tile_size==0 || x==tile_size-1 || y==tile_size-1){ // fugen weiß
+//						if(x%tile_size==0 || y%tile_size==0){ // fugen weiß
+//							
+//							col = ((255 & 0xFF) << 24) |
+//					                 ((255 & 0xFF) << 16) |
+//					                 ((255 & 0xFF) << 8)  |
+//					                 ((255 & 0xFF) << 0);								
+//							
+//							if(Math.random()<0.02){				// fugendreck :)
+//								col = ((255 & 0xFF) << 24) |
+//						                 ((200 & 0xFF) << 16) |
+//						                 ((200 & 0xFF) << 8)  |
+//						                 ((200 & 0xFF) << 0);
+//							}										
+//														
+//						}
+											
+						result.setPixel((t*tile_size)+x, (r*tile_size)+y, col);
+					}	
+				}	
+			}					
+			
+		}
+		
+		return result;
+		
+	}
+	
+	
+	public static ImageRaster generateTileTexture(int w, int h, int tilesize, int tileColor1, int tileColor2, int fugenColor) {
+		
+		ImageRaster result = new ImageRaster(w, h);
+	
+		int tile_size=tilesize;		
+		
+		int numberOfRows = h / tile_size;
+		int tilesPerRow = w / tile_size;
+	
+		
+		for(int r=0;r<numberOfRows;r++){
+			
+			for(int t=0;t<tilesPerRow;t++){
+				
+				int tileColor = Math.random()>0.5d? tileColor1 : tileColor2;
+				
+				int currentColor;
+				for(int y=0;y<tile_size;y++){
+					for(int x=0;x<tile_size;x++){
+																										
+//						if(Math.random()<0.0005){				// flecken :)
+//							col = fugenColor.getRGB();
+//						}
+						currentColor = tileColor;	
+						
+						
+						if(x==tile_size-1){ // rechter rand
+							currentColor = fugenColor;
+						}else if(x==0){ // linker rand
+							currentColor = fugenColor;
+						}
+						
+						if(y==tile_size-1){ // unterer rand
+							currentColor = fugenColor;							
+						}else if(y==0){ // oberer rand
+							currentColor = fugenColor;							
+						}		
+											
+						result.setPixel((t*tile_size)+x, (r*tile_size)+y, currentColor);
+					}	
+				}	
+			}					
+			
+		}
+		
+		return result;
+		
+	}
+	
+	
+	public static ImageRaster generateDiscoTileTexture(int w, int h, int tilesize) {
+		
+		ImageRaster result = new ImageRaster(w, h);
+	
+		int tile_size=tilesize;		
+		
+		int numberOfRows = h / tile_size;
+		int tilesPerRow = w / tile_size;
+		
+//		Color fugenColor = new Color(0, 0, 255);
+		
+		for(int r=0;r<numberOfRows;r++){
+			
+			for(int t=0;t<tilesPerRow;t++){
+				
+				int col;
+				int select = (int)(Math.random() * 6);
+				switch(select){
+					case 0: col = Color.red.getRGB(); break;
+					case 1: col = Color.green.getRGB(); break;
+					case 2: col = Color.blue.getRGB(); break;
+					case 3: col = Color.yellow.getRGB(); break;
+					case 4: col = Color.cyan.getRGB(); break;
+					case 5: col = Color.magenta.getRGB(); break;
+					default: col = Color.black.getRGB(); break;
+				}					
+				
+				
+				for(int y=0;y<tile_size;y++){
+					for(int x=0;x<tile_size;x++){
+																																
+//						if(x==tile_size-1){ // rechter rand
+//							col = fugenColor.getRGB();
+//						}else if(x==0){ // linker rand
+//							col = fugenColor.getRGB();
+//						}
+//						
+//						if(y==tile_size-1){ // unterer rand
+//							col = fugenColor.getRGB();							
+//						}else if(y==0){ // oberer rand
+//							col = fugenColor.getRGB();							
+//						}						
+											
+						result.setPixel((t*tile_size)+x, (r*tile_size)+y, col);
+					}	
+				}	
+			}					
+			
+		}
+		
+		return result;
+		
+	}
+	
+	
+	public static ImageRaster generateTileTextureNormalMap(int w, int h, int tilesize) {
+		
+		ImageRaster result = generateDefaultNormalMap(w, h);
+	
+		int tile_size=tilesize;		
+		
+		int numberOfRows = h / tile_size;
+		int tilesPerRow = w / tile_size;
+		
+		for(int r=0;r<numberOfRows;r++){
+			
+			for(int t=0;t<tilesPerRow;t++){
+				
+				// random tile displacement (x and y rotation)
+				float tile_deg_x = (float)Math.random()*6 - 3;
+				float tile_deg_y = (float)Math.random()*6 - 3;
+				
+				
+				for(int y=0;y<tile_size;y++){
+					for(int x=0;x<tile_size;x++){					
+						
+						Vector3D normal = new Vector3D(0, 0, 1); // default normal points to z direction
+																						
+						if(x==tile_size-1){ // rechter rand
+							normal = Toolbox.getYrotMatrix(-20).transform(normal);
+						}else if(x==0){ // linker rand
+							normal = Toolbox.getYrotMatrix(20).transform(normal);
+						}
+						
+						if(y==tile_size-1){ // unterer rand
+							normal = Toolbox.getXrotMatrix(-20).transform(normal);							
+						}else if(y==0){ // oberer rand
+							normal = Toolbox.getXrotMatrix(20).transform(normal);							
+						}						
+						
+						// add base displacement
+						normal = Toolbox.getRotMatrix(tile_deg_x, tile_deg_y, 0).transform(normal);						
+																							
+						int col = toColor(normal);						
+											
+						result.setPixel((t*tile_size)+x, (r*tile_size)+y, col);
+					}	
+				}	
+			}					
+			
+		}
+		
+		return result;		
+	}
+
+	
+	public static ImageRaster generateWoodTexture(int width, int height, int alpha){
+				
+		float dimension_x = 4; // x=0 -> nur längsstreifen
+		float dimension_y = 4; // regelt die streifenbreite
+		
+		ImageRaster result = new ImageRaster(width, height);
+		
+		for(int x=-width/2;x<width/2;x++){			
+			float fx = (dimension_x/(float)width)*x; // x-wert normalisieren			
+			
+			for(int y=-height/2;y<height/2;y++){								
+				float fy = (dimension_y/(float)height)*y; // y-wert normalisieren
+				
+				float noise = (float) NoiseGenerator.improvedPerlinNoise(fx, fy, 0);
+				
+				int[] rgb = TextureGenerator.generateWoodColor(fx, fy, noise);
+				
+				int col = ((alpha & 0xFF) << 24) |
+		                 ((rgb[0] & 0xFF) << 16) |
+		                 ((rgb[1] & 0xFF) << 8)  |
+		                 ((rgb[2] & 0xFF) << 0);
+				
+				result.setPixel(x+width/2, y+height/2, col);											
+			}				
+		}
+		
+		return result;
+	}
+	
+
+	
+	public static int[] generateWoodColor(float pos_x, float pos_y, float noise) {
+						
+		int brown_min_red = 70;
+		int brown_min_green = 44;
+		int brown_min_blue = 30;
+				
+		int brown_max_red = 185;
+		int brown_max_green = 122;
+		int brown_max_blue = 87;
+					
+		float value = (pos_x*pos_x + pos_y*pos_y + noise*noise);
+		//float value = (pos_x*pos_x + pos_y*pos_y  + noise*noise);
+		//float value = (Math.abs(pos_x) + Math.abs(pos_y) + noise*noise);
+				
+		value = value%1;
+		
+		int r = brown_min_red + (int)(value*(brown_max_red-brown_min_red));
+		int g = brown_min_green + (int)(value*(brown_max_green-brown_min_green));
+		int b = brown_min_blue + (int)(value*(brown_max_blue-brown_min_blue));
+		
+		return new int[]{r,g,b};
+	}
+	
+	
+	public static ImageRaster generateDefaultNormalMap(int width, int height){
+		ImageRaster normalMap = new ImageRaster(width, height);
+		
+		for (int x=0;x<width;x++) {
+			for (int y=0;y<height;y++) {
+				
+				int rgbCode = ((128 & 0xFF) << 16) |
+		                	  ((128 & 0xFF) << 8)  |
+		                	  ((255 & 0xFF) << 0);
+				
+				normalMap.setPixel(x, y, rgbCode);
+			}	
+		}
+		
+		return normalMap;
+	}
+	
+	
+	public static ImageRaster getRGBRaster(File imagefile, int w, int h) {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Image img = null;
+		try {
+			img = tk.getImage(imagefile.toURI().toURL());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}	
+			
+		return ImageHelper.getImageRaster(img, 0, 0, w, h);
+	}
+	
+	
+	public static int toColor(Vector3D normalVector){
+		int red = (int)(normalVector.x * 127) + 128;
+		int green = (int)(normalVector.y * 127) + 128;
+		int blue = (int)(normalVector.z * 127) + 128;																	
+		
+		int col =	((255 & 0xFF) << 24) |    
+					((red & 0xFF) << 16) |
+	                ((green & 0xFF) << 8)  |
+	                ((blue & 0xFF) << 0);
+		
+		return col;
+	}
+	
+}
