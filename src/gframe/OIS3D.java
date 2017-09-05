39944e7ebbaa9fbe942f3559b5c5c9746933cb61
Conrad Plake <conrad.plake@gmail.com>
2017-09-05 20:53:08 +0200

    Add files via upload

--- src/gframe/OIS3D.java
+++ src/gframe/OIS3D.java
@@ -1,6 +1,10 @@
 package gframe;
 
-public interface OIS3D{    
+
+/**
+ * An object in 3D space.
+ * */
+public interface OIS3D{
   public gframe.engine.Point3D getOrigin();  
   public gframe.engine.Matrix3D getMatrix();
   public gframe.engine.Vector3D getVelocityVector();
