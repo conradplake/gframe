0e64a0294fc37b8015d89ee1acd59ecb7877e6a2
Conrad Plake <conrad.plake@gmail.com>
2017-09-08 08:44:59 +0200

    Add files via upload

--- src/gframe/parser/WavefrontObjParser.java
+++ src/gframe/parser/WavefrontObjParser.java
@@ -5,8 +5,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
 import java.util.Hashtable;
+import java.util.Map;
 
 import gframe.engine.Face;
 import gframe.engine.Model3D;
